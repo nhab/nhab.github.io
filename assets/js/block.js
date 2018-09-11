012ffb51a4e489497c6de86166eeb723a0993f8c
nhab <n.habibollahi>
2018-09-11 18:30:44 +0430

    add

--- assets/js/block.js
+++ assets/js/block.js
@@ -2,17 +2,22 @@ function Block(title,body,code,codetype,widt="20%",hei="300px",imageSrc=null)
 {
     sStyle="padding-left:10px;padding-right:10px;vertical-align:text-top;border:1px black solid";
     sStyle+=";display:inline-block;width:"+widt+";height:"+hei+";";
-    document.write("<div style='"+sStyle+";'>");
+    var id1="dv"+Math.random()*20;
+    document.write("<div style='"+sStyle+";' id='"+id1+"'>");
     debugger;
     document.write( "<h2>" +title+"</h2>");
-    document.write(body);
+    if(body)
+        document.write(body);
     if(codetype && code)
-    document.write("<pre><code class='"+codetype+"'>"+code+"</code></pre>");
+        document.write("<pre><code class='"+codetype+"'>"+code+"</code></pre>");
     else{
         if(code)
           document.write("<pre><code >"+code+"</code></pre>");
     };
     if(imageSrc)
-        document.write("<img src='"+imageSrc+"'>");
+    {
+        //document.getElementById(id1).write("<img src='"+imageSrc+"'>");
+        document.write("<img src='"+imageSrc+"' style='width:100%'>");
+    }
     document.write("</div>");
 }
