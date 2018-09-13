e923216186cf5dfb40169741965132a924095e93
nhab <n.habibollahi>
2018-09-13 22:37:06 +0430

    add

--- assets/js/block.js
+++ assets/js/block.js
@@ -29,3 +29,36 @@ function Block(title,body,code,codetype,widt,hei,imageSrc=null,padding="7")
     }
     document.write("</div>");
 }
+function BlockHorizontally(title,body,code,codetype,widt,hei,imageSrc=null,padding="7")
+{
+    sStyle="padding-left:"+padding+"px;padding-right:"+padding+"px;vertical-align:text-top;border:1px black solid;margin-left:2px;";
+    sStyle+=";display:inline-block;overflow: auto;";
+    if(widt)
+        sStyle+=";width:"+widt+";";
+    if(hei)
+        sStyle+=";height:"+hei+";";
+    var id1="dv"+Math.random()*20;
+    document.write("<div style='"+sStyle+";' id='"+id1+"'>");
+   //header:
+    document.write( "<div style='width:100%;text-align:center;background-color:#336699;color:#ddccff;font-weight:900;"+
+                   "padding-left:2px;padding-right:2px;' >" 
+                   +title+"</div>");
+    //body:
+    if(body)
+        document.write("<div style='display:inline-block;overflow:auto;"+
+            "padding-right:7px;padding-top:7px;"+
+            "white-space:pre;clear:both;overflow: auto;overflow-wrap: break-word;vertical-align: top;'>"
+            +body+"</div>");
+    if(codetype && code)
+        document.write("<pre style='display:inline-block;overflow:auto;'><code style='overflow: auto;' class='"+codetype+"'>"+code+"</code></pre>");
+    else{
+        if(code)
+          document.write("<pre style='display:inline-block;overflow:auto;'><code style='overflow: auto;'>"+code+"</code></pre>");
+    };
+    if(imageSrc)
+    {
+        //document.getElementById(id1).write("<img src='"+imageSrc+"'>");
+        document.write("<img src='"+imageSrc+"' style='display:inline-block;overflow:auto;'>");
+    }
+    document.write("</div>");
+}
